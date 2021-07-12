df183db751cd5a90f67b124ab33e4d8679e53571
Mikey Cengija <mikeycengija@gmail.com>
2021-07-12 16:13:04 -0700

    Added a reset show hide function and texture.

---
+++ Assets/Scripts/ShowHideReset.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowHideReset : MonoBehaviour, IDeselectHandler
+{
+    [SerializeField]
+    private GameObject resetHolder;
+    private bool isShowing = true;
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        isShowing = true;
+        resetHolder.SetActive(false);
+    }
+
+    public void ShowHide()
+    {
+        if(isShowing)
+        {
+            resetHolder.SetActive(true);
+            isShowing = false;
+        }
+        else
+        {
+            resetHolder.SetActive(false);
+            isShowing = true;
+        }
+    }
+}
