050a6f651380c602bf5af9412e90028c9ed61aa3
Mikey Cengija <mikeycengija@gmail.com>
2021-07-11 23:32:36 -0700

    Code clean up for ChangeLightSettings.cs

--- Assets/Scripts/ChangeLightSettings.cs
+++ Assets/Scripts/ChangeLightSettings.cs
@@ -21,25 +21,24 @@ public class ChangeLightSettings : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void ChangeColorAndIntensity(float value)
     {
+        //Casting the float value as a byte to be able to modify RGB from 0-255.
         byte rSlider = (byte)colorsAndIntensity[0].value;
         byte gSlider = (byte)colorsAndIntensity[1].value;
         byte bSlider = (byte)colorsAndIntensity[2].value;
         byte alpha = 255;
 
+        //Create new color
         Color32 newRGBA = new Color32(rSlider, gSlider, bSlider, alpha);
 
+        //Assign color
         currentLight.color = newRGBA;
 
+        //Modify the intensity via slider.
         currentLight.intensity = colorsAndIntensity[3].value;
 
+        //Spotlight has a outer and inner spot angle slider. You can modify the two values to give you a bigger range to cover. 
         if (isSpotLight)
         {
             float outerSlider = colorsAndIntensity[4].value;
